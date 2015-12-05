e2a518fe0a76d5c64803c06a5798869b6739b64f
Pedro Augusto Sampaio Messias Ribeiro <pedro.augusto.sampaio.messias@gmail.com>
2015-12-06 00:35:28 +0200

    Mudanças a serem submetidas: 	modified:   ../README.md 	modified:   kalman_filter.f90 BHT is wright, maybe problem with BxH? 	modified:   lin_model.f90 model correct, but error model? 	modified:   synth_obs.f90 	modified:   task2p5d.f90 	modified:   values.f90

--- ex2/kalman_filter.f90
+++ ex2/kalman_filter.f90
@@ -1,16 +1,20 @@
 module kalman_filter
   use values
   implicit none
-  real(rk) :: By,K(3),xa(3),A(3,3)
 contains
-  subroutine analysis(xb,B,H,y)
+  subroutine analysis(xb,B,H,y,sig0,xa,A)
     implicit none
-    real(rk),intent(in) :: xb(3),B(3,3),H(3),y
+    real(rk),intent(in) :: xb(3),B(3,3),H(3),y,sig0
+    real(rk),intent(out) :: A(3,3),xa(3)
+    real(rk) :: K(3),By
 
-    By=dot_product(matmul(H,B),H)+sig0
+    By=(dot_product(matmul(H,B),H))+(sig0**2) !!is wright
     K=matmul(B,H)/By
     A= B-(dot_product(K,H)*B)
     xa=xb+K*(y-dot_product(H,xb))
+
+    print *,K(3),By
+    
   end subroutine analysis
 
 end module kalman_filter
--- ex2/task2p5d.f90
+++ ex2/task2p5d.f90
@@ -5,45 +5,43 @@ program task2p5d
   use stat
   implicit none
   integer :: i,years,readline
-  real(rk) :: w(3),y,B(3,3),H(3),d(3),xb(3)
+  real(rk) :: w(3),y,B(3,3),H(3),d(3),xb(3),xa(3),A(3,3)
   years=10
   xa=real([0,1,0],rk)
   H=real([1,1,0],rk)
   A=reshape(real([10,0,0,0,10,0,0,0,10],rk),shape(A))
-
   do i=1,12*years
-     print *,i
-     print *
+     !print *,i,d(3)
+     !print *
      y=obs(xa)
      B=err_model(A)
-     print *,'y= ',y
-     print *
+!!$     print *,'y= ',y
+!!$     print *
      w=[N_random(0.0_rk,0.05_rk),N_random(0.0_rk,0.01_rk),N_random(0.0_rk,0.01_rk)]
-     print *,'w= ',w
-     print *
-     xb=model(xa)
-     !xb=model(xa)+w !with noise c
-     print *,'xb = ',xb
-     print *
-
-     print *,'B= '
-     print '(3(g0,x))',transpose(B)
-     print *
-     call analysis(xb,B,H,y)
-     print *,'By= ',By
-     print *,'K= ',K
-     print *,'A= '
-     print '(3(g0,x))',transpose(A)
-     print *
-     print *,'xa= ',xa
+!!$     print *,'w= ',w
+!!$     print *
+     !xb=model(xa)
+     xb=model(xa)+w !with noise c
+!!$     print *,'xb = ',xb
+!!$     print *
+!!$     print *,'B= '
+!!$     print '(3(g0,x))',transpose(B)
+!!$     print *
+     call analysis(xb,B,H,y,sig0,xa,A)
+!!$     print *,'By= ',By
+!!$     print *,'K= ',K
+!!$     print *,'A= '
+!!$     print '(3(g0,x))',transpose(A)
+!!$     print *
+!!$     print *,'xa= ',xa
      d=xa-xb
-     !print *,i,d
-     !print *,i,K
-     !print *
-     read *,readline
-     if (readline==-1) then
-        stop
-     end if
+!!$     print *,i,d
+!!$     print *,i,K
+!!$     print *
+!!$     read *,readline
+!!$     if (readline==-1) then
+!!$        stop
+!!$     end if
 
   end do
 end program task2p5d
