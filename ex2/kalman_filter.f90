ed0862f5f75c77a6fa96fecd5935fd6b1987899e
Pedro Augusto Sampaio Messias Ribeiro <pedro.augusto.sampaio.messias@gmail.com>
2015-12-08 02:38:11 +0200

    Mudanças a serem submetidas: 	modified:   kalman_filter.f90 !pure kalman_filter fixed

--- ex2/kalman_filter.f90
+++ ex2/kalman_filter.f90
@@ -2,7 +2,7 @@ module kalman_filter
   use values
   implicit none
 contains
-  subroutine analysis(xb,B,H,y,sig0,xa,A)
+  pure subroutine analysis(xb,B,H,y,sig0,xa,A)
     implicit none
     real(rk),intent(in) :: xb(3),B(3,3),H(3),y,sig0
     real(rk),intent(out) :: A(3,3),xa(3)
@@ -18,7 +18,7 @@ contains
     xa=xb+K*(y-dot_product(H,xb))
 
     !print *,K!,By
-    
+
   end subroutine analysis
 
 end module kalman_filter
