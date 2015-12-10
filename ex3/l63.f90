bcc809f23a3d7356daf5245f065cc7ba037fe5b8
Pedro Augusto Sampaio Messias Ribeiro <pedro.augusto.sampaio.messias@gmail.com>
2015-12-11 01:21:22 +0200

    Mudanças a serem submetidas: 	modified:   l63.f90 	modified:   values.f90

--- ex3/l63.f90
+++ ex3/l63.f90
@@ -4,34 +4,27 @@ module l63
   real(rk),parameter :: sigma=10.0_rk,rho=28.0_rk,beta=8.0_rk/3.0_rk
 contains
 
-    subroutine model(x,dx)
+  pure  function model(x)
     implicit none
-    real(rk),intent(in) :: x(3)
-    real(rk),intent(out) :: dx(3)!,sigma,rho,beta
-    real(rk) :: x2(3)
-    !real(rk) :: model(3)
-
-    !sigma=10.0_rk
-    !rho=28.0_rk
-    !beta=(8.0_rk/3.0_rk)
+    real(rk),intent(in),dimension(3) :: x
+    real(rk) :: dx(3)
+    real(rk) :: model(3)
 
     dx(1)=sigma*(x(2)-x(1))
     dx(2)=x(1)*(rho-x(3))-x(2)
     dx(3)=x(1)*x(2)-beta*x(3)
-    !model=dx
-  end subroutine model
+    model=dx
+  end function model
 
-
-   subroutine err_model(x,xp,dxp,sigma,rho,beta)
+  pure function err_model(x,xp)
     implicit none
-    real(rk),intent(in) :: x(3),xp(3),sigma,rho,beta
+    real(rk),intent(in),dimension(3) :: x,xp
     real(rk) :: M(3,3)
-    real(rk),intent(out) :: dxp(3)
-    !real(rk) :: err_model(3)
+    real(rk) :: err_model(3)
 
     M=reshape([-sigma,rho-x(3),x(2),sigma,-1.0_rk,x(1),.0_rk,-x(1),-beta],shape(M))
-    dxp=matmul(M,xp)
-  end subroutine err_model
-  
-  end module l63
+    err_model=matmul(M,xp)
+  end function err_model
+
+end module l63
 
--- ex3/values.f90
+++ ex3/values.f90
@@ -2,16 +2,25 @@ module values
   implicit none
   !important constants
   integer,parameter :: rk=selected_real_kind(15,307) !select type of real
-  real(rk),parameter :: pi=4.0_rk*atan(1.0_rk),sig0=0.4_rk
+  real(rk),parameter :: pi=4.0_rk*atan(1.0_rk),sig0=sqrt(0.4_rk)
 
 contains
   !general use functions and subroutines
   real(rk) function N_random(mi,sig)
     implicit none
-    real(rk) :: mi,sig !parameters of normal distribution
+    real(rk),intent(in) :: mi,sig !parameters of normal distribution
     real(rk) :: r
     call random_number(r)
     N_random=mi+sig*(sqrt(-2*log(r))*cos(2*pi*r))
   end function N_random
+
+  pure function cross(u,v)
+    implicit none
+    real(rk),intent(in) :: u(:),v(:)
+    real(rk),allocatable :: cross(:,:)
+    integer :: i,j
+    allocate(cross(size(u),size(v)))
+    cross=reshape([((v(i)*u,i=1,size(u)),j=1,size(v))],shape(cross))
+  end function cross
   
 end module values
